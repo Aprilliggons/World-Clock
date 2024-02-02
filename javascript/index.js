035032b0cf3a170b02a1788dd18b149b4bc9cb79
Aprilliggons <149065998+Aprilliggons@users.noreply.github.com>
2024-02-02 08:04:02 -0500

    Added JavaScript to Project

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,30 @@
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM D, YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}, 1000);
 
+setInterval(function () {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector("#sydney .date");
+  let sydneyTimeElement = sydneyElement.querySelector("#sydney .time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM D, YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}, 1000);
+
+setInterval(function () {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector("#tokyo .date");
+  let tokyoTimeElement = tokyoElement.querySelector("#tokyo .time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM D, YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[<small>]");
+}, 1000);

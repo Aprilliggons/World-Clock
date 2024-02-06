a38a060c40f139d5209559dc050b9eccbfe772bd
Aprilliggons <149065998+Aprilliggons@users.noreply.github.com>
2024-02-06 07:40:14 -0500

    Added the functionality of the Select

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-setInterval(function () {
+function updateCityTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
   let losAngelesTimeElement = losAngelesElement.querySelector(".time");
@@ -7,9 +7,7 @@ setInterval(function () {
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
     "h:mm:ss [<small>]A[<small>]"
   );
-}, 1000);
 
-setInterval(function () {
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector("#sydney .date");
   let sydneyTimeElement = sydneyElement.querySelector("#sydney .time");
@@ -18,13 +16,33 @@ setInterval(function () {
   sydneyTimeElement.innerHTML = sydneyTime.format(
     "h:mm:ss [<small>]A[<small>]"
   );
-}, 1000);
 
-setInterval(function () {
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoDateElement = tokyoElement.querySelector("#tokyo .date");
   let tokyoTimeElement = tokyoElement.querySelector("#tokyo .time");
   let tokyoTime = moment().tz("Asia/Tokyo");
   tokyoDateElement.innerHTML = tokyoTime.format("MMMM D, YYYY");
   tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[<small>]");
-}, 1000);
+}
+
+setInterval(updateCityTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+          </div>`;
+}
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
